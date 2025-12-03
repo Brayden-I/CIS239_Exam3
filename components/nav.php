18070db747a24f2143adb58ba1f037b7bd892d98
BraydenI <bi1012440@otc.edu>
2025-12-03 09:57:13 -0600

    added orders button and login isolation

--- components/nav.php
+++ components/nav.php
@@ -1,17 +1,18 @@
 <nav class="navbar navbar-expand-sm" style=" background-color: #e3f2fd;" data-bs-theme="light">
     <div class="container-fluid">
 
-        <ul class="navbar-nav me-auto">
-            <li class="nav-item"><a class="nav-link" href="?view=list">List Records</a></li>
-            <li class="nav-item"><a class="nav-link" href="?view=create">Add Record</a></li>
-        </ul>
         <?php if (!empty($_SESSION['user_id'])): ?>
-            <span class="navbar-text ">Welcome, <?= htmlspecialchars($_SESSION['full_name']) ?></span>
-        <?php endif; ?>
-
-        <ul class="navbar-nav ms-auto">
-
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item"><a class="nav-link" href="?view=list">List Records</a></li>
+                <li class="nav-item"><a class="nav-link" href="?view=create">Add Record</a></li>
+            </ul>
             <?php if (!empty($_SESSION['user_id'])): ?>
+                <span class="navbar-text ">Welcome, <?= htmlspecialchars($_SESSION['full_name']) ?></span>
+            <?php endif; ?>
+
+            <ul class="navbar-nav ms-auto">
+
+
                 <li class="nav-item"><a class="nav-link" href="?view=orders">Orders</a></li>
                 <li class="nav-item"><a class="nav-link" href="?view=cart">Cart</a></li>
                 <li class="nav-item">
@@ -27,7 +28,7 @@
 
 
             <?php endif; ?>
-        </ul>
+            </ul>
 
     </div>
 </nav>
--- partials/checkout_success.php
+++ partials/checkout_success.php
@@ -1,5 +1,6 @@
 <h2>Thank you for your purchase!</h2>
 
-<p>Order for <strong></strong> completed.</p>
+<p>Order for <strong><?= htmlspecialchars($_SESSION['full_name']) ?></strong> completed.</p>
 
 <p><a href="?view=list" class="btn btn-primary">Back to Records</a></p>
+<p><a href="?view=orders" class="btn btn-primary">Go to Orders</a></p>
